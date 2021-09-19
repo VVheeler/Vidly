8898b5c5146255d5f9279dad394822b4912de419
Scott Wheeler <swheeler@gmail.com>
2021-09-19 10:38:32 -0500

    Chapter10 - Typeahead.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace Vidly
                         "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
-                        "~/Scripts/datatables/datatables.bootstrap4.js"
+                        "~/Scripts/datatables/datatables.bootstrap4.js",
+                        "~/Scripts/typeahead.bundle.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -28,7 +29,8 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
-                      "~/content/datatables/css/datatables.bootstrap4.css",
+                      "~/Content/datatables/css/datatables.bootstrap4.css",
+                      "~/Content/typeahead.css",
                       "~/Content/site.css"));
         }
     }
